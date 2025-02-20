b7bef040ae43229216cb4a0c743b50b20f43f641
rajabovvv <alexxoneee@gmail.com>
2025-02-20 18:53:08 +0900

    EJS learning

--- server.js
+++ server.js
@@ -14,15 +14,18 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
-app.get("/hello", function (req, res) {
-  res.end("<h1>HELLO WORD</h1>");
+
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/gift", function (req, res) {
-  res.end("<h1> Siz sovgalar sahifasida siz</h1>");
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function () {
+server.listen(PORT, function (req, res) {
   console.log("server is running succesfully");
 });
