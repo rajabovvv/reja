4a863f0e544f4a09044cd2d6c79532191d17492f
rajabovvv <alexxoneee@gmail.com>
2025-02-21 14:01:07 +0900

    feat: develop author routher page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/useer.json","utf8", (err,data) => {
+  if(err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data)
+  }
+})
 
 //1 Kirish Code
 app.use(express.static("public"));
@@ -14,11 +24,13 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 Routing code
-
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
+  //TODO:code with db here
 });
+
+app.get('/author', (req,res) => {
+  res.render("author",{ user: user });
+})
 
 app.get("/", function (req, res) {
   res.render("harid");
