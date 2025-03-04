d1114433f88eb229e2120a9b79017c3e4ecee992
alexone <alexxoneee@gmail.com>
2025-03-04 18:24:19 +0900

    feat: finsishing delete partog REJA

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
 console.log("web serverni boshlash");
+
 const express = require("express");
 const app = express();
 
-//MongoDB chaqirish
+// MongoDB chaqirish
 const db = require("./server").db();
+const mngodb = require("mongodb");
 
-//1 Kirish Code
+// 1 Kirish Code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//2 Session Code
-//3 Views code
+// 2 Session Code
+// 3 Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-//4 Routing code
+// 4 Routing code
 
 app.post("/create-item", (req, res) => {
-  console.log("user entered /create-item")
+  console.log("user entered /create-item");
   const new_reja = req.body.reja;
+
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    console.log(data.ops);
-    res.json(data.ops[0]);
+    if (err) {
+      console.log("Error inserting item:", err);
+      res.status(500).json({ error: "Database error" });
+    } else {
+      console.log("Inserted ID:", data.insertedId);
+      res.json({ _id: data.insertedId, reja: new_reja });
+    }
+  });
+});
+
+app.post("/delete-item", (req, res) => {
+  const id = req.body.id;
+
+  db.collection("plans").deleteOne({ _id: new mngodb.ObjectId(id) }, function (err, data) {
+    if (err) {
+      console.log("Error deleting item:", err);
+      res.status(500).json({ error: "Database error" });
+    } else {
+      res.json({ state: "success" });
+    }
   });
 });
 
 app.get("/", function (req, res) {
-  console.log("user entered /")
+  console.log("user entered /");
   db.collection("plans").find().toArray((err, data) => {
     if (err) {
-      console.log(err);
-      res.end("something went wrong");
+      console.log("Database error:", err);
+      res.end("Something went wrong");
     } else {
-      res.render("reja", { items: data }); 
+      res.render("reja", { items: data });
     }
   });
 });
--- public/browser.js
+++ public/browser.js
@@ -1,7 +1,7 @@
 console.log("FRONT END java script ishga tushdi");
 
 function itemTemplate(item) {
-    `<li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
+    return `<li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
             <span class="item-text">${item._reja}</span>
             <div> 
               <button 
@@ -15,18 +15,44 @@ function itemTemplate(item) {
                 Ochirish
               </button>
             </div>
-          </li> `
+          </li>`;
 }
+
 let createField = document.getElementById("create-field");
-document .getElementById("create-form").addEventListener("submit",function(e){
+
+document.getElementById("create-form").addEventListener("submit", function(e) {
     e.preventDefault();
 
     axios
-    .post("create-item",{reja: createField.value})
-    .then((response) => {
-        document.getElementById("item-list").insertAdjacentHTML("beforeend",itemTemplate(response.data));
-        createField.value = "";
-        createField.focus = ();
-    })
-    .catch((err) => {});
-}
+        .post("create-item", { reja: createField.value })
+        .then((response) => {
+            document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data));
+            createField.value = "";
+            createField.focus(); 
+        })
+        .catch((err) => {
+            console.log("Iltimos qaytadan xarakat"); 
+        });
+});
+
+document.addEventListener("click", function(e) {
+    // Delete operation
+    if (e.target.classList.contains("delete-me")) {
+        if (confirm("Aniq Ochirmoqchimi siz?")) {
+            axios
+                .post("/delete-item", { id: e.target.getAttribute("data-id") })
+                .then((response) => {
+                    console.log(response.data);
+                    e.target.parentElement.parentElement.remove();  // Fixed `parenElement`
+                })
+                .catch((err) => {
+                    console.log("Iltimos qaytadan xarakat");
+                });
+        }
+    }
+
+    // Edit operation
+    if (e.target.classList.contains("edit-me")) {
+        alert("siz edit tugmasini bosdingiz");
+    }
+});
